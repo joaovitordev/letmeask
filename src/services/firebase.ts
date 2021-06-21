7ac6963864bfcac3e67b5ee7356d211496942d5a
joaovitordev <jv17fc@gmail.com>
2021-06-20 21:38:14 -0300

    configurando firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDdPhu5JMcwwof9dUFFV9nl1TNpxjGXtu8",
-    authDomain: "letmeask-fe56d.firebaseapp.com",
-    databaseURL: "https://letmeask-fe56d-default-rtdb.firebaseio.com",
-    projectId: "letmeask-fe56d",
-    storageBucket: "letmeask-fe56d.appspot.com",
-    messagingSenderId: "123762722266",
-    appId: "1:123762722266:web:9c14e11343b412668aece3"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL:  process.env.REACT_APP_DATABASE_URL,
+    projectId:  process.env.REACT_APP_PROJECT_ID,
+    storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:  process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
