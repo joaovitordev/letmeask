b602c92c317ef40b9657ca6fc5625c27b1378a12
joaovitordev <jv17fc@gmail.com>
2021-06-24 22:20:24 -0300

    continuando projeto

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -2,17 +2,23 @@
 // import { useAuth } from '../hooks/useAuth';
 
 import { Link } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 
-
 import '../styles/auth.scss';
 
 export function NewRoom() {
     // const { user } = useAuth();
+
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+    }
 
     return (
         <div id="page-auth">
@@ -26,10 +32,12 @@ export function NewRoom() {
                     <img src={logoImg} alt="Logo" />
                     <h2>Criar uma nova sala</h2>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input
                             type="text"
                             placeholder="Nome da sala"
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
 
                         <Button type="submit">
